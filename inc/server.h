ae585939c7273681fd8e0a2343acfda7cdfc0612
VinTRiZz <andzig501@gmail.com>
2024-01-29 18:56:26 +0300

    Exchange packet struct added

---
+++ inc/server.h
@@ -0,0 +1,11 @@
+#ifndef SERVER_INSTANCE_H
+#define SERVER_INSTANCE_H
+
+namespace ServerInstance
+{
+
+}
+
+
+
+#endif // SERVER_INSTANCE_H
--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 
+#include "inc/exchange.h"
+
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    Exchange::Packet testPacket;
+
+    testPacket.command = "TEST!";
+    testPacket.data = "DATA";
+    testPacket.type = Exchange::PACKET_TYPES::INFO_PACKET_TYPE;
+    testPacket.isValid = true;
+
+    std::string data = Exchange::PacketConverter::convert(testPacket);
+
+    Exchange::Packet deconvPacket = Exchange::PacketConverter::convert(data);
+
+    std::cout << "Command:" << deconvPacket.command << std::endl
+              << "Type:" << deconvPacket.type << std::endl
+              << "Data:" << deconvPacket.data << std::endl
+              << "Is valid:" << deconvPacket.isValid << std::endl;
+
     return 0;
 }
