01b709a93278fdcb35c1bbfd47c9bae106d3dc99
VinTRiZz <andzig501@gmail.com>
2026-03-26 21:56:23 +0300

    Database#feature: Настройка юзера сервера

--- database/setup_database.sh
+++ database/setup_database.sh
@@ -1,16 +1,33 @@
 #! /bin/bash
 
+# Check args
 if [[ "$1" == "" || "$2" == "" || "$3" == "" || "$4" == "" ]]; then
-    echo "Usage: setup_database.sh [DB HOST] [DB PORT] [DB USER] [DB DATABASE]"
+    echo "Usage: setup_database.sh [DB HOST] [DB PORT] [DB ADMIN USER] [DB DATABASE]"
     exit 1
 fi
 
+# Get password of a admin user to execute with no asking
 read -s -p "Enter password: " PGPASSWORD
 export PGPASSWORD
 
-LOCAL_scriptDir=$(realpath $(dirname "$0"))
+# Common variables
+LOCAL_hostname=$1
+LOCAL_port=$2
+LOCAL_adminUser=$3
+LOCAL_database=$4
 
+# Execution environment
+LOCAL_psqlEnv="-h $LOCAL_hostname -p $LOCAL_port -U $LOCAL_adminUser"
+
+psql $LOCAL_psqlEnv -v CONFIGURE_SERVERUSER="server" -v CONFIGURE_DBNAME="$LOCAL_database" -f "0.sql"
+
+# Execute configuration scripts
+LOCAL_scriptDir=$(realpath $(dirname "$0"))
 for envScript in "$LOCAL_scriptDir"/*.sql; do
+    if [ "$envScript" == "$LOCAL_scriptDir/0.sql" ]; then
+        continue;
+    fi
+
     echo "Executing script: " $envScript
-    psql -h "$1" -p "$2" -U "$3" -d "$4" -f "$envScript"
+    psql $LOCAL_psqlEnv -d "$LOCAL_database" -f "$envScript"
 done
